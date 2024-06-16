96a6d7039d004c9aa77b087777dd9902db8308f4
gustavcc <ritacardoso290@gmail.com>
2024-06-15 22:25:38 -0300

    create bot wpp

---
+++ 9_BotWhatsApp/main.js
@@ -0,0 +1,19 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', message => {
+	console.log(message.body);
+});
+
+
+client.initialize();
