33a035a221e237bc0a222a8d75ef28deca19e71d
gustavcc <ritacardoso290@gmail.com>
2024-06-28 23:08:56 -0300

    continuous

--- 5_Weather/index.js
+++ 5_Weather/index.js
@@ -1,28 +1,62 @@
-// require("dotenv").config()
-// const dotenv = require('dotenv');
-// const result = dotenv.config();
+const apiKey = "57714286124cb62c52857771f79d063b";
+const APICountry = `https://flagsapi.com/BR/flat/64.png`;
 
-// const apiKey = process.env.APIKEY;
-const apiKey = "57714286124cb62c52857771f79d063b"
-const apiCountry = `https://flagsapi.com/BR/flat/64.png`;
-
-// if (result.error) {
-//     throw result.error;
-// }
-
-// console.log(result.parsed);
-
-const apiCity = document.getElementById('city-input');
+const cityInput = document.getElementById('city-input');
 const searchBtn = document.getElementById('search');
 
+const cityElement = document.getElementById('city');
+const countryElement = document.getElementById('country');
+const temperatureElement = document.getElementById('temperatura');
+const descriptionElement = document.getElementById('description');
+const weatherIconElement = document.getElementById('weather-icon');
+const umidifyElement = document.getElementById('umidify');
+const windElement = document.getElementById('wind');
+const weatherData = document.getElementById('weather-data');
 
-const showWeatherData = (city) => {
-    
+//  obter dados da api a partir do nome da cidade
+const getWeatherData = async(city) => {
+    const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const response = await fetch(apiWeatherUrl);
+    const data = response.json();
+
+    return data;
+}
+
+//  Mostra dados da api
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    umidifyElement.innerText = parseInt(data.main.humidity)
+    ;
+    temperatureElement.innerText = data.main.temp
+    ;
+    windElement.innerText = data.wind.speed;
+    descriptionElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        'src',
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute(
+        'src',
+        `https://flagsapi.com/${data.sys.country}/flat/64.png`
+    );
+
+    weatherData.style.display = 'flex';
 }
 
 searchBtn.addEventListener('click',(e)=>{
     e.preventDefault();
 
-    const city = apiCity.value;
-    console.log(city);
+    const city = cityInput.value;
+    showWeatherData(city);
+})
+
+cityInput.addEventListener('keydown', (e) => {
+    if (e.code == 'Enter') {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
 })
